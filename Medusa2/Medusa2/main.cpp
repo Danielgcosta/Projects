8998b36bf32b32691d0040c76dd14dea3868cfa6
Daniel <Daniel@R2D2>
2016-10-29 21:00:13 -0200

    sensoring test update

--- Medusa2/Medusa2/main.cpp
+++ Medusa2/Medusa2/main.cpp
@@ -6,11 +6,29 @@ using namespace std;
 
 int main() {
 	int answer;
-	cout << "Olá, quantos anos você tem?" << endl;
+	cout << "Olá, quantos sensores você vai usar?" << endl;
 	cin >> answer;
-
 	cout << "Sua resposta foi: " << answer << endl;
-	cin >> answer;
+	
+	char confirm = '!';
+	while (confirm != 'S' && confirm != 'N' && confirm != 's' && confirm != 'n')
+	{
+		cout << "Confirma a quantidade? (S/N)" << endl;
+		cin >> confirm;
+		if (confirm != 'S' && confirm != 'N' && confirm != 's' && confirm != 'n') {
+			cout << "Resposta " << confirm << " inválida." << endl;
+		}
+		else if (confirm == 'n' || confirm == 'N') {
+			cout << "Digite a quantidade desejada: " << endl;
+			cin >> answer;
+			cout << "Sua resposta foi: " << answer << endl;
+			confirm = '!';
+		}
+	}
+
+	cout << "Quantidade de sensores corretamente configurada." << endl;
+	int exitFlag;
+	cin >> exitFlag;
 }
 
 
