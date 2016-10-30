b3577112d06b90fa3e0c704cd5bb2bf9db83e8ea
Daniel <Daniel@R2D2>
2016-10-30 07:36:25 -0200

    Implementation of Thermal sensor temperatura calculation: Thermal::evaluate()

--- Medusa2/Medusa2/Thermal.cpp
+++ Medusa2/Medusa2/Thermal.cpp
@@ -18,6 +18,13 @@ string Thermal::getType()
 
 void Thermal::evaluate()
 {
-	// Implementar o retorno do sensor de temperatura
+	// Calibrate sensor
+	// 1st wire ----[GND]
+	// 2nd wire ----[DATA] 
+	//			\---[4,7K]----[5V]
+	
+	// For example, if analog pin reads 250-700 when temps are 1.4C to 44.1C
+	//float x = map(analogRead(_port), 250, 700, 14, 441)/10;  
+	//_value /= 10.0;          // divide by 10; map() uses integers
 	_value = 1;
 }
