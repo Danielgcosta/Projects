981217d99fa45379bee44e2af7d83c4b28ebd46a
Daniel <Daniel@R2D2>
2016-10-30 05:51:03 -0200

    Implementation of PH::evaluate()

--- Medusa2/Medusa2/PH.cpp
+++ Medusa2/Medusa2/PH.cpp
@@ -17,9 +17,14 @@ string PH::getType()
 
 void PH::evaluate()
 {
-	// E = E0 + RT*ln(alphaH+)/F = E0 - 2.303*RT*pH/F
+	// E = E0 + RT*ln(alphaH+)/F = E0 - 2.303*R*T*pH/F
 	// R = Ideal Gas Constant
 	// T = Temperature in Kelvin
 	// F = Faraday constant
+
+	// Hence we must calibrate E0 with the neutral solution
+	const float E0 = 1; //dummy value for now, overwrite with calibration value
+	//_value = (E0-E)*F/(2.303*R*T)
+
 	_value = 2;
 }
